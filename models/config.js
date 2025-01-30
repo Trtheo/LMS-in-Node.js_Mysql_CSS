31fd5bf7c65c55de487d8fb15cc764a537663091
Theophile <niyigabatheo10@gmail.com>
2025-01-30 23:34:49 +0200

    change for database

--- models/config.js
+++ models/config.js
@@ -2,7 +2,7 @@ var mysql      = require('mysql');
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
-  password : 'Niyigaba2002@',
+  password : 'Your MySQL password',
   database : 'library_management_system'
 });
 
