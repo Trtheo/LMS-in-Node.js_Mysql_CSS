ec3dfd4ad2f49387d23542ad37b3bbba547e8852
Theophile <niyigabatheo10@gmail.com>
2025-01-30 23:43:52 +0200

    Updated start script for production

--- models/config.js
+++ models/config.js
@@ -2,7 +2,7 @@ var mysql      = require('mysql');
 var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
-  password : 'Your MySQL password',
+  password : 'Niyigaba2002@',
   database : 'library_management_system'
 });
 
